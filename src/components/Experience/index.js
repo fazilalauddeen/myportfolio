da589932f1d0293ef9aa8f22a559fd42334b3477
Fazil <145034281+fazilalauddeen@users.noreply.github.com>
2024-02-13 01:50:46 +0530

    Update index.js

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -8,7 +8,6 @@ import HeroSection from "./components/HeroSection";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
