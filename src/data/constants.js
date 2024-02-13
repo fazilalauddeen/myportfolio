329953b4501e442c46abd65c5f205f7fd680846f
Fazil <145034281+fazilalauddeen@users.noreply.github.com>
2024-02-13 17:49:13 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/fazilalauddeen",
+    github: "https://github.com",
     resume:
       "https://drive.google.com/file/d/1RVpd57TOJkzvDhUx6kutZG1GXehkFLBg/view?usp=drivesdk",
     linkedin: "https://www.linkedin.com/fazil-alauddeen",
